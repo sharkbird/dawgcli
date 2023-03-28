f2f4ba5cecc1358a974c112c0e1a6a7b52c610ed
sharkbird <jason@sharkbird.com>
2023-03-27 21:19:11 -0500

    Now reads from file, and generates a module the packed trie representation.

--- command/generatePackedDAWG.js
+++ command/generatePackedDAWG.js
@@ -1,8 +1,26 @@
+const process=require("process");
+const Trie = require("dawg-lookup").Trie;
+const PTrie = require("dawg-lookup/lib/ptrie").PTrie;
+const fs = require("fs");
+
+//const data = require(`${process.cwd()}/word.js`);
+
+
 function generatePackedDAWG(fileName, moduleName) {
-if (moduleName==="same") {
-    moduleName=fileName.replace(/\.[^/.]+$/, "");
-}
-console.log(fileName + moduleName);
+    if (moduleName==="same") {
+        moduleName=fileName.replace(/\.[^/.]+$/, "");
+    };
+    
+    let words = fs.readFileSync(fileName).toString(); 
+    const trie = new Trie(words);
+    const packedTrie=trie.pack();
+    
+    fs.writeFileSync(`${moduleName}.js`,`data="${packedTrie}";module.exports = data`);
+    
+    
+    //   const pTrie=new PTrie(data);
+    //   console.log(pTrie.isWord("cat")); 
+    //   console.log(process.cwd());
 }
 
 module.exports = generatePackedDAWG
